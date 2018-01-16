c96f989095854b54c8b5407e198e6d369fddb704
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-01-17 00:07:02 +0600

    Add files via upload

---
+++ INSERT INTO.sql
@@ -0,0 +1,6 @@
+-- This command inserts columns into a table
+
+INSERT INTO Students 
+VALUES ('Shah Newaj Shishir',151382014,'CSE','38th',5);
+INSERT INTO Students 
+VALUES ('Sayeed Anwar',151382019,'CSE','38th',5);
