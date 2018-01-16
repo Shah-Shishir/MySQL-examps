76abeaaaec5e612b231fb52ec7c7c670ea5cf310
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-01-17 01:10:04 +0600

    Delete CREATE TABLE.sql

--- CREATE TABLE.sql
+++
@@ -1,9 +0,0 @@
--- This command creates a new table
-
-CREATE TABLE Students (
-    Name varchar(20) NOT NULL,
-    ID int(10) NOT NULL,
-    Department varchar(10) NOT NULL,
-    Batch varchar(5) NOT NULL,
-    NumberofCourses int(2) NOT NULL
-);
