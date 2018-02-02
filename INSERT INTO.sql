00d221208a248105dd5c6a67a1ffc5fe40984c30
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-02-02 23:11:15 +0600

    Delete INSERT INTO.sql

--- INSERT INTO.sql
+++
@@ -1,6 +0,0 @@
--- This command inserts columns into a table
-
-INSERT INTO Students 
-VALUES ('Shah Newaj Shishir',151382014,'CSE','38th',5);
-INSERT INTO Students 
-VALUES ('Sayeed Anwar',151382019,'CSE','38th',5);
